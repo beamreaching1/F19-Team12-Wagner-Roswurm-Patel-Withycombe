a162df8fe13d03600cc72a1590304ea37cfe0b7c
Raecaug <ppatel2@clemson.edu>
2019-10-01 16:42:47 -0400

    Lots of backend code

--- update_username.php
+++ update_username.php
@@ -20,26 +20,23 @@ if(mysqli_connect_error())
     //Capture variables
     $user = mysqli_real_escape_string($connection, 
     filter_input(INPUT_POST,'user'));
-    $newpass = mysqli_real_escape_string($connection,
-    filter_input(INPUT_POST,'pass'));
-    $newpass2 = mysqli_real_escape_string($connection,
-    filter_input(INPUT_POST,'pass2'));
+    $newuser = mysqli_real_escape_string($connection,
+    filter_input(INPUT_POST,'newuser'));
+    $newuser2 = mysqli_real_escape_string($connection,
+    filter_input(INPUT_POST,'newuser2'));
 
-    //Check that the passwords match
-    if($newpass == $newpass2)
+    //Check that the usernames match
+    if($newuser == $newuser2)
     {
-        //Generate hash of inputted password
-        $options = ['cost' => 10,];
-        $new_hash = password_hash($newpass, PASSWORD_DEFAULT, $options);
 
-        $update = "UPDATE Password_Hash SET 
-        HASH = '$new_hash' WHERE id IN (SELECT 
+        $update = "UPDATE Account SET 
+        username = '$newuser' WHERE id IN (SELECT 
         id FROM Account WHERE username = '$user')";
 
         mysqli_query($connection, $update);
 
     } else {
-        echo "The entered passwords do not match. Please
+        echo "The entered usernames do not match. Please
         try again.";
     }
 }
