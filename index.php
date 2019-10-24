00f4737bf1fa3add2e71364a010dc0bd4edf95b6
beamreaching1 <caydenw@g.clemson.edu>
2019-10-24 17:35:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,5 @@
    <script type="text/javascript" src="/js/jquery.min.js"></script>
    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
    <body>
-       <br>
-       <br>
-      <p>Redirecting to Login Page</p>
    </body>
 </html>
