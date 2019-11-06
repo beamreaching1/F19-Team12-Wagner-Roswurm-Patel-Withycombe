0111a11c9287bddf215a2ee514906bc0964df87c
Raecaug <ppatel2@clemson.edu>
2019-11-06 17:16:49 -0500

    added category info

--- ebay_populate.php
+++ ebay_populate.php
@@ -93,9 +93,10 @@ if ($resp->ack == "Success") {
     $link  = $item->viewItemURL;
     $title = $item->title;
     $price = $item->sellingStatus->convertedCurrentPrice;
+    $category = $item->primaryCategory->categoryName;
 
     // For each SearchResultItem node, build a link and append it to $results
-    $results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td><td>\$$price</td></tr>";
+    $results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td><td>\$$price</td><td>$category</td></tr>";
   }
 }
 // If the response does not indicate 'Success,' print an error
