bbe6865864e2f527980fd69a48d8e6e94b90c315
beamreaching1 <caydenw@g.clemson.edu>
2019-11-12 16:28:38 -0500

    Update display.php

--- display.php
+++ display.php
@@ -16,8 +16,11 @@ if(mysqli_connect_error())
     administrator.<br \>\n";
 
 } else {
-  $tableName = "Account";
-  $tableName = mysqli_real_escape_string($connection, $_POST['select']);
+  if(isset($_POST)){
+    $tableName = mysqli_real_escape_string($connection, $_POST['select']);
+  }else {
+    $tableName = "Account";
+  }
   $sql = "SHOW COLUMNS FROM $tableName";
   $res = $connection->query($sql);
   
