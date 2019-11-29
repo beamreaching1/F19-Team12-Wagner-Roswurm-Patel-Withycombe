b21817f3a86a0f0d549a2da72b6cdc2af96caef3
beamreaching1 <caydenw@g.clemson.edu>
2019-11-28 23:22:29 -0500

    Update forgotpassword.php

--- forgotpassword.php
+++ forgotpassword.php
@@ -53,9 +53,9 @@ if(isset($_POST['submit'])){
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
-    <link href="/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link href="/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
     <title>Password Reset</title>
        
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
