b8b1f3601b7a5519badec606f935459df5b7e314
beamreaching1 <caydenw@g.clemson.edu>
2019-11-29 02:13:31 -0500

    Update market.php

--- market.php
+++ market.php
@@ -60,11 +60,7 @@ if(mysqli_connect_error())
     <script type="text/javascript" src="/js/jquery.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-	
-	  <!-- Bootstrap css -->
+	<!-- Bootstrap css -->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<!-- Style css -->
 	<link rel="stylesheet" type="text/css" href="market.css">
