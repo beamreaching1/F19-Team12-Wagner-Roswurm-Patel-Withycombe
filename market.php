9add05b448ba9f6d60828994bb8688b810cdec03
beamreaching1 <caydenw@g.clemson.edu>
2019-11-29 12:08:40 -0500

    Update market.php

--- market.php
+++ market.php
@@ -88,7 +88,7 @@ if(mysqli_connect_error())
 	</div>
 	<hr>
 	<div class="text-center">
-		<button type="button" class="btn btn-primary"><a href="cart.php">Cart</a></button>
+		<button type="button" class="btn btn-primary"><a style="color: #ffffff; text-decoration: none;" href="cart.php">Cart</a></button>
 	</div>
 
 
