ae6fe6ffdad537c1eb3f44c0272ec48613867f32
beamreaching1 <caydenw@g.clemson.edu>
2019-12-08 22:33:20 -0500

    Update points.php

--- points.php
+++ points.php
@@ -54,6 +54,18 @@ mysqli_close($connection);
 
 <!DOCTYPE HTML>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Admin Suite
+	</title>
+       
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+	<link rel="stylesheet" type="text/css" href="adminsuite.css">
+</head>
 <body>
 
 <div id="nav-placeholder"></div>
